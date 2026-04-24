fb285ed2961b78e558e3c6e20fad5000d09c60da
joaovcamargom <joaovcamargom@gmail.com>
2026-04-23 21:21:52 -0300

    Exercicio 2 - declaracao de variaveis e seus tipos

---
+++ declararVariaveis.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+
+using namespace std;
+
+int main(){
+    /*
+    Variaveis são semelhantes a gavetas do guarda roupa, são espaços vazios esperando por algo de valor, 
+    mas as gavetas esperam por algo específico para guardar. As variaveis mais comuns são:
+
+    int - Armazena valores inteiros (Ex.: -999, -998 ..., 0, 1, 2, 3 ...)
+    double - Neste tipo de variavel, costuma armazenar números decimais (Ex.: 1.88, 3.14, 8.2 ...)
+    float - tem a mesma funcionalidade do double, mas ele armazena 7 digitos ao invés de 17
+    char - Armazena caracter (Ex.: 'a', 'b', 'c' ...)
+    string - o tipo string tem a funcionalidade de armazenar texto, ou muitas palavras
+    bool - armazena somente dois tipos de valores, true (verdadeiro/1) ou false (falso/0)
+
+    Variavel segue o seguinte padrão: 
+    type nameVar = value;
+
+    Obs: Estou utilizando declaração de variavel do tipo camelCase
+    */
+
+    int idade = 25;
+    double peso = 74.89;
+    float altura = 182.0;
+    char genero = 'M';
+    string nome = "João V. C. Montes";
+    bool habilitado = true;
+
+    //Comando de saida para exibir os valores das variaveis.
+    // cout << variavelAqui
+
+    return 0;
+}
